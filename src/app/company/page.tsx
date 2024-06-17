47b086fd8efc8e6b9eb50152faf3f7b965a078a6
fuji-tacoma <y.fujita@fourm.co.jp>
2024-06-17 02:24:09 +0000

    Edit CompanyPage

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -1,11 +1,114 @@
 import React from 'react'
 import Hero from '../_components/Hero'
+import Image from "next/image";
+
+ /* 「沿革」に表示する点と線の設定 */
+const TimelineDot: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-2 h-2 bg-gray-500 rounded-full"></div>
+      <div className="h-12 w-px bg-gray-300"></div>
+    </div>
+  );
+};
+
+const TimelineEND: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-2 h-2 bg-gray-500 rounded-full"></div>
+    </div>
+  );
+};
+
 
 function page() {
   return (
     <div>
-      <Hero title='Company' text='会社概要' />
-      <h1>!!!!!!!!!!!</h1>
+      <Hero title='Company' text='会社概要'  /> 
+      <main className='CompanyTop'>
+        <div className='Greetings'> {/* 「ご挨拶」の親要素 */}
+          <div className='text-center pt-10 pb-10'>
+            <h1 className='text-3xl font-bold'>ご挨拶</h1>
+          </div>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-4 px-80'>
+            <Image
+              src="A-force_horizon.svg"
+              alt=""
+              width={265}
+              height={260}
+            />
+            <div>
+              <p className='text-gray-700 ml-2'>
+              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='TimeLine'> {/* 「沿革」の親要素 */}
+          <div>
+            <div className='text-center pt-10 pb-10'>
+              <h1 className='text-3xl font-bold'>沿革</h1>
+            </div>
+            <div className='grid grid-cols-3 px-96 items-center'>
+              <div className="grid grid-rows-7 gap-4">
+                <div className="flex items-center justify-end h-12">YY:MM</div>
+                <div className="flex items-center justify-end h-12">YY:MM</div>
+                <div className="flex items-center justify-end h-12">YY:MM</div>
+                <div className="flex items-center justify-end h-12">YY:MM</div>
+                <div className="flex items-center justify-end h-12">YY:MM</div>
+                <div className="flex items-center justify-end h-12">YY:MM</div>
+                <div className="flex items-center justify-end h-12">YY:MM</div>
+              </div>
+              <div className="grid grid-rows-7 gap-0">
+                <TimelineDot />
+                <TimelineDot />
+                <TimelineDot />
+                <TimelineDot />
+                <TimelineDot />
+                <TimelineDot />
+                <TimelineEND />
+              </div>
+              <div className="grid grid-rows-7 gap-4">
+                <div className="h-12 flex items-center">テキスト</div>
+                <div className="h-12 flex items-center">テキスト</div>
+                <div className="h-12 flex items-center">テキスト</div>
+                <div className="h-12 flex items-center">テキスト</div>
+                <div className="h-12 flex items-center">テキスト</div>
+                <div className="h-12 flex items-center">テキスト</div>
+                <div className="h-12 flex items-center">テキスト</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='AboutUs'> {/* 「会社概要」の親要素 */}
+          <div>
+            <div className='text-center pt-10 pb-10'>
+              <h1 className='text-3xl font-bold'>会社概要</h1>
+            </div>
+            <div className="grid grid-rows-19 px-96 gap-1">
+                <div className="h-12 flex items-center">会社名　　　：　テキスト</div>
+                <div className="h-px w-full bg-gray-300"></div>
+                <div className="h-12 flex items-center">所在地　　　：　テキスト</div>
+                <div className="h-px w-full bg-gray-300"></div>
+                <div className="h-12 flex items-center">設立　　　　：　テキスト</div>
+                <div className="h-px w-full bg-gray-300"></div>
+                <div className="h-12 flex items-center">売上　　　　：　テキスト</div>
+                <div className="h-px w-full bg-gray-300"></div>
+                <div className="h-12 flex items-center">代表　　　　：　テキスト</div>
+                <div className="h-px w-full bg-gray-300"></div>
+                <div className="h-12 flex items-center">従業員　　　：　テキスト</div>
+                <div className="h-px w-full bg-gray-300"></div>
+                <div className="h-12 flex items-center">許認可　　　：　テキスト</div>
+                <div className="h-px w-full bg-gray-300"></div>
+                <div className="h-12 flex items-center">所属団体　　：　テキスト</div>
+                <div className="h-px w-full bg-gray-300"></div>
+                <div className="h-12 flex items-center">顧問　　　　：　テキスト</div>
+                <div className="h-px w-full bg-gray-300"></div>
+              </div>
+
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,10 +1,100 @@
-import React from 'react'
-import Hero from '../_components/Hero'
+import React from 'react';
+import Hero from '../_components/Hero';
+import './service.css';
+import Image from "next/image";
+import Link from 'next/link';
+/*import { motion } from framer - motion;*/
+
+
 
 function Services() {
   return (
-    <Hero title='Services' text='サービス' />
-  )
+    <div> {/* このdivが全ての要素を囲む親要素となる */}
+      <Hero title='Services' text='サービス' desc='どんな事業をやっているのか？' />
+      <div>
+        <main className='service_top'>
+          <div className='container mx-auto p-4'>
+            <div className='grid grid-cols-1 md:grid-cols-2 pt-8 gap-4'>
+              <div>
+                <h1 className='text-2xl font-bold mb-4 '>ミッション</h1>
+                <h2 className='text-gray-700 mb-4'>お客様の成功と笑顔を広げるベストデリバリーカンパニー</h2>
+                <p className='text-gray-700 ml-4'>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                </p>
+              </div>
+              <div className='bg-white p-6 rounded shadow-md'>
+                <img src='A-force_horizon.svg' alt='' />
+              </div>
+              <hr className='col-span-2 my-4 border-gray-300'></hr>
+              <div>
+                <h1 className='text-2xl font-bold mb-4 '>エクセレントサービス</h1>
+                <h1 className='text-gray-700 mb-4'>エクセレントサービス小見出し</h1>
+                <p className='text-gray-700 ml-4'>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                </p>
+              </div>
+              <div className='bg-white p-6 rounded shadow-md'>
+                <img src='A-force_horizon.svg' alt='' />
+              </div>
+              <hr className='col-span-2 my-4 border-gray-300'></hr>
+              <div>
+                <h1 className='text-2xl font-bold mb-4 '>SI事業</h1>
+                <h2 className='text-gray-700 mb-4'>SI事業小見出し</h2>
+                <p className='text-gray-700 ml-4'>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                </p>
+                <button className='bg-black text-white py-2 px-10 mt-4 ml-4 rounded-md' >
+                  詳しく見る
+                </button>
+              </div>
+              <div className='items-center justify-center bg-white p-6 rounded shadow-md'>
+                <img src="A-force_horizon.svg" alt='' />
+              </div>
+              <hr className='col-span-2 my-4 border-gray-300'></hr>
+              <div>
+                <h1 className='text-2xl font-bold mb-4 '>コンサルティング事業</h1>
+                <h2 className='text-gray-700 mb-4'>コンサルティング事業小見出し</h2>
+                <p className='text-gray-700 ml-4'>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                </p>
+                <button className='bg-black text-white py-2 px-10 mt-4 ml-4 rounded-md' >
+                  詳しく見る
+                </button>
+              </div>
+              <div className='items-center justify-center bg-white p-6 rounded shadow-md'>
+                <img src="A-force_horizon.svg" alt='' />
+              </div>
+              <hr className='col-span-2 my-4 border-gray-300'></hr>
+              <div>
+                <h1 className='text-2xl font-bold mb-4 '>教育事業</h1>
+                <h2 className='text-gray-700 mb-4'>教育事業小見出し</h2>
+                <p className='text-gray-700 ml-4'>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                  テキストテキストテキストテキストテキストテキストテキスト<br></br>
+                </p>
+                <button className='bg-black text-white py-2 px-10 mt-4 ml-4 rounded-md' >
+                  詳しく見る
+                </button>
+              </div>
+              <div className='items-center justify-center bg-white p-6 rounded shadow-md'>
+                <img src="A-force_horizon.svg" alt='' />
+              </div>
+              <hr className='col-span-2 my-4 border-gray-300'></hr>
+            </div>
+          </div >
+        </main >
+      </div >
+    </div >
+  );
 }
 
-export default Services
+export default Services;
