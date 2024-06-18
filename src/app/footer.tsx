b50e8e86650aa0819d938376b4bc9b84f9945c49
Nao Takaoka <n.takaoka@fourm.co.jp>
2024-06-18 18:17:56 +0900

    フッター画像・pagetop配置完了。リスト未実装。

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -3,11 +3,26 @@ import Image from 'next/image';
 
 function footer() {
 	return (
-		<div className="bg-black p-4 h-auto">
-			<Image className="ml-7 pt-4" src="Alogo.svg" alt="" width={200} height={970} ></Image>
-			<div className="relative flex" >
-				<Image className="ml-6 mt-6 mr-4" src="footer_A-force.svg" alt="" width={150} height={970}></Image>
-				<Image className="ml-6 mt-6 mr-4" src="footer_copyright.svg" alt="" width={200} height={970}></Image>
+		<div className="bg-black p-4 h-200 flex">
+			<div className="w-1/2">
+					<div className="relative flex ml-20">
+						<Image className="pt-4" src="Alogo.svg" alt="" width={200} height={970}  />
+						<Image className="absolute bottom-0 left-40" src="footer_catchcopy.svg" alt="" width={200} height={970} />
+					</div>
+					<div className="relative flex ml-20 mt-6 mr-4" >
+						<Image src="footer_A-force.svg" alt="" width={150} height={970} />
+						<Image className="ml-6" src="footer_copyright.svg" alt="" width={200} height={970} />
+						<Image className="ml-6" src="footer_privacy.svg" alt="" width={50} height={970} />
+					</div>
+					<div className="ml-20 mt-2">
+						<Image src="footer_facebook.svg" alt="" width={30} height={970} />
+					</div>
+			</div>
+			<div className="w-1/2 flex justify-end">
+				<div className="relative flex flex-col">
+					<Image className="absolute right-10 mt-4" src="footer_chevronup.svg" alt="" width={30} height={970}></Image>
+					<p className="right-10 mt-8 mr-6 text-white">Page Top</p>
+				</div>
 			</div>
 		</div>
 	);
