4006ff4ecf8a305d4405d965e426ff5e5121c403
NorioSaito <n.saito@a-force.co.jp>
2024-06-18 14:39:49 +0000

    製品詳細画面実装

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Link from 'next/link';
 import Hero from '../_components/Hero';
 
 function Products() {
-	return <Hero title="Prodcts" text="製品紹介" />;
+	return (
+		<>
+			<Hero title="Prodcts" text="製品紹介" />
+			<Link href="/products/celf">CELF</Link>
+			<Link href="/products/salesforce">Sales Force</Link>
+		</>
+	);
 }
 
 export default Products;
