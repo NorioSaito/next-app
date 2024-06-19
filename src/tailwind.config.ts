0e7bdcb57ad7323cd97de01d162eca109ca14ebd
NorioSaito <n.saito@a-force.co.jp>
2024-06-19 09:33:40 +0000

    font-size を 1rem = 16px に設定

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
 				'gradient-conic':
 					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
 			},
+			fontSize: {
+				base: '1.6rem',
+			},
 		},
 	},
 	plugins: [],
