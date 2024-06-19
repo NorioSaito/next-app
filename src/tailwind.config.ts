c9df04520285207a3e7937ef1b2fcf95a0a4de0b
Kaede Kuriyama <k.kuriyama@a-force.co.jp>
2024-06-19 05:44:38 +0000

    HP使用カラーコード　tailwindに設定

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -2,34 +2,44 @@ import type { Config } from 'tailwindcss';
 import type { PluginAPI } from 'tailwindcss/types/config';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [
-    function ({ addUtilities }: PluginAPI) {
-      const newUtilities = {
-        '.text-outline-black': {
-          color: 'white',
-          textShadow: `
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			colors: {
+				'af-orange': '#FF5C00', // オレンジ
+				'af-black': '#2B2C32', // 黒
+				'af-white': '#ffffff', // 白
+				'af-gray': '#F9F9F9', // グレー
+				'af-blue': '#0057FF', // ブルー
+				'af-yellow': '#FFD400', // 黄色
+				'af-green': '#EBFF00', // 緑（色が黄色と同じ）
+			},
+		},
+	},
+	plugins: [
+		function ({ addUtilities }: PluginAPI) {
+			const newUtilities = {
+				'.text-outline-black': {
+					color: 'white',
+					textShadow: `
             -1px -1px 0 #000,
             1px -1px 0 #000,
             -1px 1px 0 #000,
             1px 1px 0 #000`,
-        },
-      };
-      addUtilities(newUtilities);
-    },
-  ],
+				},
+			};
+			addUtilities(newUtilities);
+		},
+	],
 };
 
 export default config;
