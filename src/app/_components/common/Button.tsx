7860e5434b064de8cd624dc9c13da4ea70d3bbbf
Kaede Kuriyama <k.kuriyama@a-force.co.jp>
2024-06-26 04:56:36 +0000

    ボタンコンポーネント　サイズ、画像修正

--- src/app/_components/common/Button.tsx
+++ src/app/_components/common/Button.tsx
@@ -1,18 +1,25 @@
 'use client';
 
 import React from 'react';
+import Image from 'next/image';
 import style from '../../assets/css/common.module.css';
 
 function Button(props: { title: string; link: string }) {
 	return (
 		<a
 			href={props.link}
-			className={`${style.btn} ${style.bgleft} grid grid-cols-3 w-15 border text-black rounded-full pl-4 pr-2 py-2`}
+			className={`${style.btn} ${style.bgleft} grid grid-cols-3 w-[320px] h-[63px] border text-black rounded-full bg-af-white pl-[48px] pr-[10px]`}
 		>
-			<div className="col-span-2">{props.title}</div>
+			<div className="flex col-span-2 text-start items-center text-af-xs">{props.title}</div>
 			<div className="grid items-center contents-cente justify-end">
-				<div className="font-bold text-center align-middle w-7 h-7 rounded-full bg-orange-600">
-					＞
+				<div className="flex items-center justify-center font-bold text-center align-middle w-[45px] h-[45px] rounded-full bg-af-orange">
+					<Image
+						className="flex items-center justify-start"
+						alt=""
+						width={4}
+						height={10}
+						src="v.svg"
+					/>
 				</div>
 			</div>
 		</a>
