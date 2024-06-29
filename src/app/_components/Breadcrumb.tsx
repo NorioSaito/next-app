b1ccbf86c80fde2b373cf63fdbf2799fd7e12cf9
Norio Saito <norio.saito@sharpeye.co.jp>
2024-06-29 11:26:54 +0000

    product_detail merge

--- src/app/_components/Breadcrumb.tsx
+++ src/app/_components/Breadcrumb.tsx
@@ -1,17 +1,32 @@
-import PATHS from '@/constants/paths';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
-function Breadcrumb() {
+// Breadcrumb コンポーネントの定義
+const Breadcrumb = () => {
+	const pathName = usePathname();
+	let joinedPath = '';
+
 	return (
-		<div className="mt-5">
-			<Link href={PATHS.INDEX}>Top</Link>
-			&gt;
-			<Link href={PATHS.PRODUCTS}>Products</Link>
-			&gt;
-			<Link href={PATHS.PRODUCTS}>CELF</Link>
+		<div className="flex items-center text-sm px-4 w-full">
+			<Link href="/">
+				<p className="text-gray-500 hover:text-gray-600" />
+				Home
+				<p />
+			</Link>
+			{pathName.split('/').map((path, index) => {
+				if (path) {
+					joinedPath += `${path}/`;
+					return (
+						<Link key={index} href={`/${joinedPath}`}>
+							<span className="text-gray-500 mx-2">/</span> {path}
+						</Link>
+					);
+				}
+				return <></>;
+			})}
 		</div>
 	);
-}
+};
 
 export default Breadcrumb;
